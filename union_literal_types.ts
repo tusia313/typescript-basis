660f34a5d59d5a9a1c45a82eb52b3afb9688d89a
Marta Łoskot <loskotmarta@gmail.com>
2026-05-22 21:01:44 +0200

    type guards

--- union_literal_types.ts
+++ union_literal_types.ts
@@ -1,48 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// Make sure you replace all of the `unknown` types with the appropriate
-// union types.
-// Create a union type that represents either a string or an array of strings
-
-type StringOrStringArray = string | string[];
-
-const fruitName: StringOrStringArray = "Apple";
-// This should be valid
-const fruitList: StringOrStringArray = ["Apple", "Banana", "Pear"];
-
-// Create a union type that represents an array of numbers and strings
-type NumberAndStringArray = (number | string)[];
-const assortedItems: NumberAndStringArray = ["Orange", 5, 2, "Strawberry", 1];
-
-// Create a union type that includes literal types for the days of the week
-// Don't use a string here
-type DaysOfTheWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
-
-const today: DaysOfTheWeek = "Friday";
-const tomorrow: DaysOfTheWeek = "Saturday";
-
-// This will have a red squiggle if the type
-// below it is valid. You want to get rid of
-// the red squiggle
-// @ts-expect-error
-const fruitDay: DaysOfTheWeek = "Apple";
-
-// Create a union type that includes these two interfaces
 interface Fruit {
-    name: string;
-    sweetness?: number;
+  name: string;
+  sweetness: number;
+  color: unknown;
 }
-interface Vegetable {
-    name: string;
-    hasSeeds: boolean;
-}
-type Food = Fruit | Vegetable;
 
-const apple: Food = { name: "Apple", sweetness: 80 };
-const onion: Food = { name: "Vegetable", hasSeeds: false };
-
-// Fix the problems in this function without changing the parameter or return types
-function getSweetness(fruit?: Fruit): number {
-    if (!fruit) return 0;
-    const sweetness = fruit.sweetness || 0;
-    return sweetness;
+// Add the necessary return types and implementation for these
+// user-defined type guards
+function isString(maybeString: unknown): maybeString is string {
+  return typeof maybeString === "string";
 }
+function isFruit(maybeFruit: unknown): maybeFruit is Fruit {
+  if ((typeof maybeFruit !== "object") | (typeof maybeFruit === null)) {
+    return false;
+  }
+  return name in maybeFruit && sweetness in maybeFruit && color in maybeFruit;
+}
+function assertIsFruit(maybeFruit: unknown): asserts maybeFruit is Fruit {
+  if (!isFruit(maybeFruit)) {
+    throw new Error("Dane muszą być obiektem!");
+  }
+}
+
+// Don't change anything in this function
+function checkFruit(fruit: unknown) {
+  if (isFruit(fruit)) {
+    if (isString(fruit.color)) {
+      console.log(fruit.color.toUpperCase());
+    }
+  }
+  assertIsFruit(fruit);
+
+  console.log(`This fruit is ${fruit.name}`);
+}
+
+
